06a9e0d637e579057267bbe70eec19c8aff86922
Joe Kinney <joekinney53@gmail.com>
2019-07-23 19:49:09 -0500

    added schema and seeds files in DB folder

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO devices (device_name, device_status) VALUES ('Lights', 'off');
+INSERT INTO devices (device_name, device_status) VALUES ('Front-Door', 'Locked/closed');
+INSERT INTO devices (device_name, device_status) VALUES ('Garage-Door', 'Locked/closed');
+INSERT INTO devices (device_name, device_status) VALUES ('Alarms', 'off');
