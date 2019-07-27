1cd610bae479a1afdd4d3eb6d70798e5edbfb0cc
andym@ANDYMULLER <andym@ANDYMULLER>
2019-07-27 08:38:30 -0500

    Create security.js

---
+++ models/security.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+    var Security = sequelize.define("Security", {
+      device_name: DataTypes.INTEGER,
+      device_status: DataTypes.BOOLEAN
+    });
+  
+    Security.associate = function(models) {
+      // We're saying that a Security should belong to an Author
+      // A Security can't be created without an Author due to the foreign key constraint
+      Security.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    
+     
+  
+    return Security;
+  
+  };
+  
+  
+    
+  
