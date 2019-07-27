2a4844f14217f2a186cba30f6860394c0f758204
andym@ANDYMULLER <andym@ANDYMULLER>
2019-07-27 08:40:36 -0500

    Updated models

--- models/security.js
+++ models/security.js
@@ -1,26 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
-    var Security = sequelize.define("Security", {
-      device_name: DataTypes.INTEGER,
-      device_status: DataTypes.BOOLEAN
+  var Security = sequelize.define("Security", {
+    device_name: DataTypes.INTEGER,
+    device_status: DataTypes.BOOLEAN
+  });
+
+  Security.associate = function(models) {
+    // We're saying that a Security should belong to an Author
+    // A Security can't be created without an Author due to the foreign key constraint
+    Security.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
     });
-  
-    Security.associate = function(models) {
-      // We're saying that a Security should belong to an Author
-      // A Security can't be created without an Author due to the foreign key constraint
-      Security.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    
-     
-  
-    return Security;
-  
   };
-  
-  
-    
-  
+
+  return Security;
+};
---
+++ models/user.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: {
+      defaultValue: "password",
+      type: DataTypes.STRING
+    }
+  });
+
+  User.associate = function(models) {
+    // Associating User with Posts
+    // When an User is deleted, also delete any associated Posts
+    User.hasMany(models.Security, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: {
+      defaultValue: "password",
+      type: DataTypes.STRING
+    }
+  });
+
+  User.associate = function(models) {
+    // Associating User with Posts
+    // When an User is deleted, also delete any associated Posts
+    User.hasMany(models.Security, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
