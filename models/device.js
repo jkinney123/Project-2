75bdca055518bff523760b142c0885a1ebac458a
Kiya Abdulahi <kiya.abdulahi@gmail.com>
2019-07-27 12:56:33 -0500

    fixed null issue

---
+++ models/device.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+  var Device = sequelize.define("Device", {
+    device: DataTypes.STRING,
+    status: DataTypes.BOOLEAN,
+  });
+  return Device;
+};
+
+
+  
