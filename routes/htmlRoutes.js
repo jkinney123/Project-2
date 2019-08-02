a0b37145c5d79490db2569875b3cb48beb4541c9
LM Train <layconmm@msn.com>
2019-08-02 14:22:43 -0500

    Resolve file conflics

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,6 @@
-module.exports = function (app) {
+var db = require("../models");
+
+module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Device.findAll({}).then(function(dbDevices) {
@@ -73,8 +75,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
-
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -12,7 +13,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
 // Routes
@@ -32,7 +38,11 @@ if (process.env.NODE_ENV === "test") {
 // Starting the server, syncing our models
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
-    console.log(`Server started on port ${PORT}`);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
 
